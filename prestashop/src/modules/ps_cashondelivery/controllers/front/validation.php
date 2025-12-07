c77f9e958f4a61338fd07e648abdfaa11433f3cf
VV01T3K <wojteks.access+github@gmail.com>
2025-12-07 19:22:38 +0000

    feat: modify status after cach on delivery payment to instantly generate invoice

--- prestashop/src/modules/ps_cashondelivery/controllers/front/validation.php
+++ prestashop/src/modules/ps_cashondelivery/controllers/front/validation.php
@@ -64,7 +64,7 @@ class Ps_CashondeliveryValidationModuleFrontController extends ModuleFrontContro
 
         $this->module->validateOrder(
             (int) $this->context->cart->id,
-            (int) Configuration::getGlobalValue(Ps_Cashondelivery::CONFIG_OS_CASH_ON_DELIVERY),
+            (int) Configuration::get('PS_OS_PAYMENT'),
             (float) $this->context->cart->getOrderTotal(true, Cart::BOTH),
             $this->module->displayName,
             null,
